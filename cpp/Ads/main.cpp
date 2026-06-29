c60bf579b0f3d642bab417c0486b1133d773b973
Pogdie <PogdieDev@Gmail.com>
2026-06-29 18:30:38 +0200

    added Ads program source

---
+++ cpp/Ads/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct ad {
+  int watchedAds{};
+  int clickedAdsPercentage{};
+  int averageEarnings{};
+};
+
+ad getAdInfo() {
+  ad temp{};
+  std::cout << "Watched ads?: ";
+  std::cin >> temp.watchedAds;
+  std::cout << "Clicked ads percentage?: ";
+  std::cin >> temp.clickedAdsPercentage;
+  std::cout << "Average earnings per ad?: ";
+  std::cin >> temp.averageEarnings;
+  return temp;
+}
+
+void printAdInfo(const ad &ad) {
+  std::cout << "\nWatched ads: " << ad.watchedAds << '\n';
+  std::cout << "Clidked ads percentage: " << ad.clickedAdsPercentage << '\n';
+  std::cout << "Avarege earnings: " << ad.averageEarnings << '\n';
+
+  double totalEarnings{ad.watchedAds * (ad.clickedAdsPercentage / 100.0) *
+                       ad.averageEarnings};
+  std::cout << "total earnings: " << totalEarnings << '\n';
+}
+
+int main() {
+  printAdInfo(getAdInfo());
+  return 0;
+}
