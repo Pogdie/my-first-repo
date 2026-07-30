9d8f6225eb44fff5ba3b90e5e864d9669a086e36
Pogdie <PogdieDev@gmail.com>
2026-07-30 19:54:44 +0200

    updated BubbleSort with some comments

--- cpp/BubbleSort/main.cpp
+++ cpp/BubbleSort/main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 
-void sortArray(int array[], int length) {
+void sortArray(int array[], int length) { // it sorts the array
+
   for (int i{0}; i < length - 1; ++i) {
     bool swapped{false};
+
     for (int currentIndex{0}; currentIndex < length - 1 - i; ++currentIndex) {
-      if (array[currentIndex] > array[currentIndex + 1]) {
+      if (array[currentIndex] >
+          array[currentIndex + 1]) { // if the current index is greater than the
+                                     // next one it swaps
         int temp{0};
         temp = array[currentIndex + 1];
         array[currentIndex + 1] = array[currentIndex];
         array[currentIndex] = temp;
-        swapped = true;
+        swapped = true; // updates if bool swapped to true
       }
     }
-    if (!swapped)
+    if (!swapped) // check if swapped and if so break
       break;
   }
 }
 
-void printArray(int array[], int length) {
+void printArray(int array[], int length) { // it prints all array
   for (int i{0}; i < length; ++i)
-    std::cout << array[i];
+    std::cout << array[i] << ' ';
   return;
 }
 
